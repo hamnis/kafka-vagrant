0755aa87d52b62b44c67b663a34ae697c1812c71
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-02-10 09:08:34 +0100

    remove fetch, replaced by vagrant

--- fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-VERSION=0.8.0
-BASE=kafka_2.8.0-${VERSION}
-
-wget http://apache.komsys.org/kafka/0.8.0/${BASE}.tar.gz -O /tmp/${BASE}.tar.gz
-tar zxfv /tmp/${BASE}.tar.gz 
-ln -sf ${BASE} kafka
